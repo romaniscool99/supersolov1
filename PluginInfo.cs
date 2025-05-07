ff8322dc6647acb89e622977e151066311aa3b7a
Roman :D <clippedbynarzox@gmail.com>
2025-05-06 20:31:56 -0400

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,8 +3,8 @@
     public class PluginInfo
     {
         public const string GUID = "org.iidk.gorillatag.iimenu";
-        public const string Name = "ii's Stupid Menu";
-        public const string Description = "Created by @goldentrophy with love <3";
+        public const string Name = "SuperSolo Menu";
+        public const string Description = "Created by @realsoloo (discord) with love <3";
         public const string Version = "6.0.0";
     }
 }
